08c17dc6470d4d246c4ea80bcfb553df20658396
patoowa <patoowa@gmail.com>
2023-10-17 18:03:45 +0200

    adding shuffling function

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.scss'
 
-function App() {
 
+const imagesArray = [
+  {"src": "/img/crab.png"},
+  {"src": "/img/elephant.png"},
+  {"src": "/img/giraffe.png"},
+  {"src": "/img/lion.png"},
+  {"src": "/img/parrot.png"},
+  {"src": "/img/penguin.png"}
+]
+
+function App() {
+const [cards, setCards] = useState([])
+
+
+  const shuffleImg = () => {
+    const shuffledImg = [...imagesArray, ...imagesArray]
+    .sort(() => Math.random() - 0.5)
+    .map((img)=>({...img, id: uuidv4()}))
+
+    setCards(shuffledImg);
+  }
+  console.log(cards)
   return (
     <>
       <div className = "Game">
         <h1>Memory Game</h1>
-        <button>Start</button>
+        <button onClick = {shuffleImg}>Start</button>
       </div>
     </>
   )
